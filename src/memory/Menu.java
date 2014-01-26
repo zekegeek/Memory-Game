53fc276a5b052fbfc9b9648385e983fcac7390fe
Catherine Roebuck <c.agnes.c@gmail.com>
2014-01-25 20:57:40 -0800

    Created board class

---
+++ src/memory/Menu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package memory;
+
+/**
+ *
+ * @author geojson
+ */
+public class Menu {
+    boolean active;
+    
+    void isActive() {
+        System.out.println("The menu is" + active);
+    }
+}
