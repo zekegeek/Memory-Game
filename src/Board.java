b110ee561a770f51a3a467faa2fa93287b79abfc
Catherine Roebuck <c.agnes.c@gmail.com>
2014-01-25 20:30:30 -0800

    Created board class

---
+++ src/Board.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author geojson
+ */
+public class Board {
+    int columnCount = 4;
+    int rowCount = 4;
+    
+ public Board() {
+    }
+    
+ public void displaySize() {
+        System.out.println("\n\tThe board is " + this.rowCount + " by " 
+                           + this.columnCount + " in size.");  
+}
+ 
+}
