ef06a90532ed91fedbf82fd51990d807638df460
Sony Style <Sony Style@SonyStyle-VAIO>
2014-03-13 18:42:00 -0600

    L9 - Menu class

--- src/memory/Menu.java
+++ src/memory/Menu.java
@@ -6,28 +6,14 @@
 
 package memory;
 
+import java.util.Scanner;
+
 /**
  *
- * @author geojson
+ * @author Roebuck and Cook
  */
 public abstract class Menu {
-    /*
-    public Menu() {
-    }
-    
-    public Menu(String[][] menuItems) {
-    }
-    
-    public String[][] getMenuItems() {
-    }
-    
-    public void setMenuItems(String[][] menuItems) {
-    }
-    protected final void display() {
-    }
-    private boolean validCommand(String command) {
-    }
-    protected final */
+   
     private String[][] menuItems = null;
     public Menu() {
     }
@@ -45,5 +31,60 @@ public abstract class Menu {
     public void setMenuItems(String[][] menuItems) {
         this.menuItems = menuItems;
     }
-    
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < this.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+
+    private boolean validCommand(String command) {
+        String[][] items = this.menuItems;
+
+        for (String[] item : this.menuItems) {
+            if (item[0].equals(command)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+protected final String getCommand() {
+ 
+        Scanner inFile = new Scanner(System.in);
+        String command;
+        boolean valid = false;
+        do {
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            valid = validCommand(command);
+            //if (!validCommand(command)) {
+                //System.out.println("Invalid command: Please enter a valid command.");
+                //continue;}
+                
+        } while (!valid);
+        
+        return command;
+    }
 }
+
+ /*
+    public Menu() {
+    }
+    
+    public Menu(String[][] menuItems) {
+    }
+    
+    public String[][] getMenuItems() {
+    }
+    
+    public void setMenuItems(String[][] menuItems) {
+    }
+    protected final void display() {
+    }
+    private boolean validCommand(String command) {
+    }
+    protected final */
