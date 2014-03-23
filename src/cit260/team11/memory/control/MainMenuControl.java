08d56af10424737ad3f0fc4f3d1da9577701d49f
Catherine <Catherine@192.168.0.16>
2014-03-22 23:40:35 -0700

    created Help enum

--- src/cit260/team11/memory/control/MainMenuControl.java
+++ src/cit260/team11/memory/control/MainMenuControl.java
@@ -9,6 +9,7 @@ package cit260.team11.memory.control;
 import cit260.team11.memory.build.Game;
 import cit260.team11.memory.menus.GamePreferenceView;
 import cit260.team11.memory.menus.HelpMenuView;
+import cit260.team11.memory.interfaces.MainMenuInterface;
 import java.io.Serializable;
 import java.util.Scanner;
 
@@ -16,21 +17,23 @@ import java.util.Scanner;
  *
  * @author Catherine
  */
-public class MainMenuControl implements Serializable{
+public class MainMenuControl implements Serializable, MainMenuInterface {
+    @Override
     public void displayGamePreferenceMenu() {
         Game game = new Game();
         GamePreferenceView gamePreferenceMenu = new GamePreferenceView();
         gamePreferenceMenu.display();
     }
 
+    @Override
      public void displayHelpMenu() {
         HelpMenuView helpMenu = new HelpMenuView();
         //helpMenu.getInput();
     }
      
+     @Override
      public void exit() {
      System.out.println("Thanks for playing!");
      System.exit(0);
      }
-     
 }
